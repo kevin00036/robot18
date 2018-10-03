a05b8d6ac81d19698a8bccd20424fe54ec54f365
Kai-Chi Huang <kchuang@cs.utexas.edu>
2018-10-03 15:41:16 -0500

    HW3 final

--- core/python/behaviors/combination.py
+++ core/python/behaviors/combination.py
@@ -23,7 +23,7 @@ class Ready(Task):
             self.finish()
 
 WDIS = 200
-MAX_VEL = 0.5
+MAX_VEL = 0.3
 
 ptime = 0.0
 P = 0
@@ -41,9 +41,11 @@ dD = 0
 stage_flag = 0
 kick_frame = 0
 finish_time = 0
+align_time = 0
 
 
 def reset_params():
+    global ptime, P, I, D, aP, aI, aD, dP, dI, dD, stage_flag, WDIS, kick_frame, finish_time, align_time
     P = 0
     I = 0
     D = 0
@@ -72,9 +74,16 @@ def print_flag(stage_flag):
     else:
         print('9 finish!!!')
 
+def clip(x, mx):
+    if x >= mx:
+        x = mx
+    if x <= -mx:
+        x = -mx
+    return x
+
 class Playing(Task):
     def run(self):
-        global ptime, P, I, D, aP, aI, aD, dP, dI, dD, stage_flag, WDIS, kick_frame, finish_time
+        global ptime, P, I, D, aP, aI, aD, dP, dI, dD, stage_flag, WDIS, kick_frame, finish_time, align_time
         print_flag(stage_flag)
     
         commands.setStiffness()
@@ -82,9 +91,9 @@ class Playing(Task):
         if stage_flag == 0:
             ball = memory.world_objects.getObjPtr(core.WO_BALL)
             if not ball.seen:
-                commands.setWalkVelocity(0, 0, 0.5)
+                commands.setWalkVelocity(0, 0, MAX_VEL)
             else:
-                commands.setWalkVelocity(0, 0, 0)
+                # commands.setWalkVelocity(0, 0, 0)
                 stage_flag = 1
     
         # Walk ball
@@ -104,11 +113,7 @@ class Playing(Task):
             D = P - preP
         
             C = -1.5*P/1000
-        
-            if C > 0.5:
-                C = 0.5
-            if C < -0.5:
-                C = -0.5
+            C = clip(C, MAX_VEL)
         
             ##########################################
         
@@ -121,17 +126,14 @@ class Playing(Task):
             aD = aP - apreP
         
             aC = -aP/2
-            #print(aC)
+            aC = clip(aC, MAX_VEL)
         
-            if aC > 0.5:
-                aC = 0.5
-            if aC < -0.5:
-                aC = -0.5
-        
-            if ball.visionDistance > WDIS:
+            if not ball.seen:
+                stage_flag = 0
+            elif ball.visionDistance > WDIS:
                 commands.setWalkVelocity(C, 0, aC)
             else:
-                commands.setWalkVelocity(0, 0, 0)
+                # commands.setWalkVelocity(0, 0, 0)
                 reset_params()
                 goal = memory.world_objects.getObjPtr(core.WO_OWN_GOAL)
                 if not goal.seen:
@@ -157,6 +159,8 @@ class Playing(Task):
             D = P - preP
         
             C = -1.5*P/1000
+            C = clip(C, MAX_VEL)
+            C = clip(C, MAX_VEL)
 
             if C > 0.5:
                 C = 0.5
@@ -174,22 +178,20 @@ class Playing(Task):
             aD = aP - apreP
         
             aC = -aP/2
+            aC = clip(aC, MAX_VEL)
         
-            if aC > 0.5:
-                aC = 0.5
-            if aC < -0.5:
-                aC = -0.5
-        
-            dC = 0.4
+            dC = MAX_VEL
             m = (C * C + dC * dC) ** 0.5
             rat = m / min(m, MAX_VEL)
             C, dC = C * rat, dC * rat
 
             goal = memory.world_objects.getObjPtr(core.WO_OWN_GOAL)
-            if not goal.seen:
+            if not ball.seen:
+                stage_flag = 0
+            elif not goal.seen:
                 commands.setWalkVelocity(C, dC, aC)
             else:
-                commands.setWalkVelocity(0, 0, 0)
+                # commands.setWalkVelocity(0, 0, 0)
                 reset_params()
                 stage_flag = 3
     
@@ -213,11 +215,7 @@ class Playing(Task):
             D = P - preP
         
             C = -1.5*P/1000
-        
-            if C > 0.5:
-                C = 0.5
-            if C < -0.5:
-                C = -0.5
+            C = clip(C, MAX_VEL)
         
             ##########################################
         
@@ -230,11 +228,7 @@ class Playing(Task):
             aD = aP - apreP
         
             aC = -aP/2
-        
-            if aC > 0.5:
-                aC = 0.5
-            if aC < -0.5:
-                aC = -0.5
+            aC = clip(aC, MAX_VEL)
         
             ##########################################
 
@@ -247,26 +241,24 @@ class Playing(Task):
             dD = dP - dpreP
         
             dC = dP 
-        
-            if dC > 0.5:
-                dC = 0.5
-            if dC < -0.5:
-                dC = -0.5
+            dC = clip(dC, MAX_VEL)
 
             ##########################################
             m = (C * C + dC * dC) ** 0.5
             rat = m / min(m, MAX_VEL)
             C, dC = C * rat, dC * rat
                 
-            if goal.visionBearing > 0.15 or goal.visionBearing < -0.15 or ball.visionBearing > 0.15 or ball.visionBearing < -0.15:
+            if not ball.seen:
+                stage_flag = 0
+            elif not goal.seen:
+                stage_flag = 2
+            elif abs(goal.visionBearing) > 0.15 or abs(ball.visionBearing) > 0.15:
                 commands.setWalkVelocity(C, dC, aC)
                 print(dC)
             else:
-                commands.setWalkVelocity(0, 0, 0)
+                # commands.setWalkVelocity(0, 0, 0)
                 reset_params()
                 stage_flag = 4
-    
-    
     
     
         # Walk goal
@@ -291,11 +283,7 @@ class Playing(Task):
             D = P - preP
     
             C = -1.5*P/1000
-    
-            if C > 0.5:
-                C = 0.5
-            if C < 0:
-                C = 0
+            C = clip(C, MAX_VEL)
     
             aT = 0
             aV = ball.visionBearing
@@ -306,11 +294,7 @@ class Playing(Task):
             aD = aP - apreP
         
             aC = -aP/2
-        
-            if aC > 0.5:
-                aC = 0.5
-            if aC < -0.5:
-                aC = -0.5
+            aC = clip(aC, MAX_VEL)
         
             ##########################################
 
@@ -323,22 +307,19 @@ class Playing(Task):
             dD = dP - dpreP
         
             dC = dP 
-        
-            if dC > 0.5:
-                dC = 0.5
-            if dC < -0.5:
-                dC = -0.5
+            dC = clip(dC, MAX_VEL)
 
             ##########################################
             
             
-            if goal.visionBearing > 0.3 or goal.visionBearing < -0.3 or ball.visionBearing > 0.3 or ball.visionBearing < -0.3:
-                commands.setWalkVelocity(0, 0, 0)
+            if abs(goal.visionBearing) > 0.3 or abs(ball.visionBearing) > 0.3:
+                # commands.setWalkVelocity(0, 0, 0)
                 reset_params()  
                 stage_flag = 1
             elif goal.distance < 1100 and ball.visionDistance < WDIS and abs(goal.visionBearing) < 0.15 and abs(ball.visionBearing) < 0.15:
-                commands.setWalkVelocity(0, 0, 0)
+                # commands.setWalkVelocity(0, 0, 0)
                 reset_params()
+                align_time = self.getTime()
                 stage_flag = 5
             else:
                 commands.setWalkVelocity(C, 0, aC)
@@ -358,15 +339,11 @@ class Playing(Task):
             
             preP = P
             P = T - V
-            I = I + P
+            I = I + P * dtime
             D = P - preP
         
-            C = -1.*P/1000
-        
-            if C > 0.3:
-                C = 0.3
-            if C < -0.3:
-                C = -0.3
+            C = (-1. * P - I * 0.1)/1000
+            C = clip(C, 0.3)
 
             if P > 0:
                 C = 0
@@ -378,15 +355,11 @@ class Playing(Task):
             
             dpreP = dP
             dP = dT - dV
-            dI = dI + dP
+            dI = dI + dP * dtime
             dD = dP - dpreP
         
-            dC = -dP * 1.5
-        
-            if dC > 0.3:
-                dC = 0.3
-            if dC < -0.3:
-                dC = -0.3
+            dC = -dP * 1.5 - dI * 0.1
+            dC = clip(dC, 0.3)
 
             ##########################################
             aT = 0
@@ -398,25 +371,22 @@ class Playing(Task):
             aD = aP - apreP
         
             aC = -aP * 0.4
-        
-            if aC > 0.5:
-                aC = 0.5
-            if aC < -0.5:
-                aC = -0.5
+            aC = clip(aC, MAX_VEL)
+
             ##########################################
             m = (C * C + dC * dC) ** 0.5
             rat = m / min(m, MAX_VEL)
             C, dC = C * rat, dC * rat
                 
             # print('ABS', abs(dV - dT), 'V', V)
-            if abs(dV - dT) < 0.05 and V < 130:
+            if (abs(dV - dT) < 0.05 and V < 130) or (self.getTime() - align_time >= 15):
                 commands.setWalkVelocity(0, 0, 0)
                 reset_params()
                 stage_flag = 6
                 kick_frame = 0
             else:
                 commands.setWalkVelocity(C, dC, aC)
-                print(C, dC, aC, 'XD', V, dV, aV)
+                print(C, dC, aC, 'XD\n', V, dV, aV, dI)
     
         # Kick
         if stage_flag == 6:
@@ -431,6 +401,6 @@ class Playing(Task):
         # Finish
         if stage_flag == 9:
             commands.stand()
-            if self.getTime() - finish_time >= 1:
+            if self.getTime() - finish_time >= 7:
                 stage_flag = 0
-                self.finish()
+                # self.finish()
