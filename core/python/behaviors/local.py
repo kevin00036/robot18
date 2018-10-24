8a9f4159e4ee039da956105802dfeacb79d28856
hhsinping <hsinping@cs.utexas.edu>
2018-10-23 23:31:08 -0500

    state machine

--- core/python/behaviors/local.py
+++ core/python/behaviors/local.py
@@ -38,43 +38,66 @@ def clip(x, mx):
         x = -mx
     return x
 
+
+
+state = 0
+
 headth = 0.01
 dth = 0.02
+
+start_time = 0
+
 class Playing(Task):
     def run(self):
-        global headth, dth
+        robot = memory.world_objects.getObjPtr(memory.robot_state.WO_SELF)
+        global headth, dth, state, start_time
 
-        commands.setHeadPan(headth, 0.1)
-        
-        headth = headth + dth
-        if headth > 1.8:
-            dth = -dth
-        if headth < -1.8:
-            dth = -dth
+        if state == 0:
+            print('stage 0 : flying')
+        elif state == 1:
+            print('stage 1 : turn')
+        elif state == 2:
+            print('stage 2 : walk')
             
 
-        robot = memory.world_objects.getObjPtr(memory.robot_state.WO_SELF)
-        # print(self.loc.x, self.loc.y, self.orientation)
+        if robot.flying:
+            state = 0
 
-        x = robot.loc.x
-        y = robot.loc.y
-        th = robot.orientation
+        if state == 0:
+            if not robot.flying:
+                state = 1
+                start_time = self.getTime()
+            
+        elif state == 1:
 
-        dis = math.sqrt(x*x+y*y)
-        vabs = min(0.3, dis)
-        vw = normAngle(math.atan2(-y,-x) - th)
-        if abs(vw) < 0.05:
-            vw = 0
+            commands.setWalkVelocity(0, 0, 0.4)
+            if self.getTime() - start_time > 15.0:
+                state = 2
 
-        vx = (+ (-x) * math.cos(th) + (-y) * math.sin(th)) / dis * vabs
-        vy = (- (-x) * math.sin(th) + (-y) * math.cos(th)) / dis * vabs
+        elif state == 2:       
+            commands.setHeadPan(headth, 0.1)
+            headth = headth + dth
+            if headth > 1.8:
+                dth = -dth
+            if headth < -1.8:
+                dth = -dth
 
-        # vx = clip(vx, 0.2)
-        # vy = clip(vy, 0.2)
-        if dis > 100:
-            vw = clip(vw, 0.2)
-        else:
-            vw = 0.
-        # vw = 0.
+            x = robot.loc.x
+            y = robot.loc.y
+            th = robot.orientation
 
-        commands.setWalkVelocity(vx, vy, vw)
+            dis = math.sqrt(x*x+y*y)
+            vabs = min(0.3, dis)
+            vw = normAngle(math.atan2(-y,-x) - th)
+            if abs(vw) < 0.05:
+                vw = 0
+
+            vx = (+ (-x) * math.cos(th) + (-y) * math.sin(th)) / dis * vabs
+            vy = (- (-x) * math.sin(th) + (-y) * math.cos(th)) / dis * vabs
+
+            if dis > 100:
+                vw = clip(vw, 0.2)
+            else:
+                vw = 0.
+
+            commands.setWalkVelocity(vx, vy, vw)
